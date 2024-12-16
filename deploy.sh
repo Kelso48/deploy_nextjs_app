a3652ceddb5c2aa1cca12058341a99f0497c75b3
Ubuntu <ubuntu@ip-10-107-0-25.ap-south-1.compute.internal>
2024-12-16 02:11:50 +0000

    First commit

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ export NVM_DIR=$HOME/.nvm;
 cd /home/ubuntu/deploy_nextjs_app
 git pull origin master
 nvm use v16
+export PATH="$PATH:$(yarn global bin)"
 yarn install &&
 yarn build &&
 pm2 restart next_hello_world_app
